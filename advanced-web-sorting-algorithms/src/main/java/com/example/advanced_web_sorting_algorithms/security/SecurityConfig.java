74acf8c437a552786b5c40fcef9c7a8e890ebe64
Gideon Agbosu <gideon.agbosu@amalitech.com>
2024-11-26 15:45:26 +0000

    Fixed cors and resolved access flight blocks from frontend

--- advanced-web-sorting-algorithms/src/main/java/com/example/advanced_web_sorting_algorithms/security/SecurityConfig.java
+++ advanced-web-sorting-algorithms/src/main/java/com/example/advanced_web_sorting_algorithms/security/SecurityConfig.java
@@ -3,15 +3,22 @@ package com.example.advanced_web_sorting_algorithms.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.List;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpMethod.*;
 
 @Configuration
 @EnableWebSecurity
@@ -26,15 +33,18 @@ public class SecurityConfig {
         return httpSecurity.csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(request -> {
                     CorsConfiguration configuration = new CorsConfiguration();
-                    configuration.setAllowedOrigins(List.of("*"));
+                    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+                    configuration.setAllowedMethods(List.of(GET.name(), POST.name(), PUT.name(), DELETE.name()));
+                    configuration.setAllowedHeaders(List.of(AUTHORIZATION, CONTENT_TYPE));
                     return configuration;
                 }))
                 .authorizeHttpRequests(requests -> requests
                         .requestMatchers("/api/users/**")
                         .permitAll()
                         .anyRequest()
-                        .permitAll()
+                        .authenticated()
                 )
+                .sessionManagement(sessions-> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
